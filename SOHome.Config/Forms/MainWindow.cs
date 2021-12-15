6cee962053cd1bcd11fd54ca1c95e0858e0a845b
Osni Pezzini Junior <osni.pezzini@gmail.com>
2021-12-15 01:14:45 -0300

    Ajustado estilo padrão do projeto

---
+++ SOHome.Config/Forms/MainWindow.cs
@@ -0,0 +1,58 @@
+using System;
+using Gtk;
+using UI = Gtk.Builder.ObjectAttribute;
+
+namespace SOHome.Config.Forms
+{
+    class MainWindow : Window
+    {
+        [UI] private Entry txtDbPassword = null;
+        [UI] private Button btnSave = null;
+        [UI] private ComboBoxText cmbThemes = null;
+
+        private int _counter;
+
+        public MainWindow() : this(new Builder("MainWindow.glade")) { }
+
+        private MainWindow(Builder builder) : base(builder.GetRawOwnedObject("MainWindow"))
+        {
+            builder.Autoconnect(this);
+            Icon = Module.Icon;
+
+            DeleteEvent += Window_DeleteEvent;
+            btnSave.Clicked += SaveButton_Clicked;
+
+            cmbThemes.Changed += CmbThemes_Changed;
+        }
+
+        private void CmbThemes_Changed(object sender, EventArgs e)
+        {
+            try
+            {
+                Module.ApplyTheme(cmbThemes.ActiveText);
+            }
+            catch (Exception ex)
+            {
+                MessageDialog md = new MessageDialog(this,
+                DialogFlags.DestroyWithParent, MessageType.Error,
+                ButtonsType.Close, "Tema incompatível");
+                md.Run();
+                md.Destroy();
+
+                Module.ApplyTheme();
+            }
+            
+        }
+
+        private void Window_DeleteEvent(object sender, DeleteEventArgs a)
+        {
+            Application.Quit();
+        }
+
+        private void SaveButton_Clicked(object sender, EventArgs a)
+        {
+            _counter++;
+            txtDbPassword.Text = "Hello World! This button has been clicked " + _counter + " time(s).";
+        }
+    }
+}
---
+++ SOHome.Config/Module.cs
@@ -0,0 +1,55 @@
+﻿using Gdk;
+
+using System.IO;
+using System.Reflection;
+
+namespace SOHome.Config
+{
+    internal class Module
+    {
+        private static Pixbuf _iconInstance;
+        public static Stream GetResource(string resourceName)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var fileName = $"{assembly.GetName().Name}.Resources.{resourceName}";
+            return assembly.GetManifestResourceStream(fileName);
+        }
+
+        public static Pixbuf Icon
+        {
+            get
+            {
+                if (_iconInstance == null)
+                {
+                    var imgResource = GetResource("logo-sohome.svg");
+                    _iconInstance = new Pixbuf(imgResource);
+                }
+                return _iconInstance;
+            }
+        }
+
+        public static void ApplyTheme(string themeName = null)
+        {
+            // Based on this Link http://awesome.naquadah.org/wiki/Better_Font_Rendering
+
+            // Get the Global Settings
+            var setts = Gtk.Settings.Default;
+            // This enables clear text on Win32, makes the text look a lot less crappy
+            setts.XftRgba = "rgb";
+            // This enlarges the size of the controls based on the dpi
+            //setts.XftDpi = 96;
+            // By Default Anti-aliasing is enabled, if you want to disable it for any reason set this value to 0
+            //setts.XftAntialias = 0
+            // Enable text hinting
+            setts.XftHinting = 1;
+            //setts.XftHintstyle = "hintslight"
+            setts.XftHintstyle = "hintfull";
+
+            // Load the Theme
+            Gtk.CssProvider css_provider = new Gtk.CssProvider();
+            //css_provider.LoadFromPath("themes/DeLorean-3.14/gtk-3.0/gtk.css")
+            css_provider.LoadFromPath($"Assets/Themes/{themeName ?? "Sweet"}/gtk-3.0/gtk.css");
+            Gtk.StyleContext.AddProviderForScreen(Gdk.Screen.Default, css_provider, 800);
+        }
+    }
+}
